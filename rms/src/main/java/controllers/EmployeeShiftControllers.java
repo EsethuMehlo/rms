5d5fabbca721606b672927518b852dd364bed1f7
esethumehlo <216125723@mycput.ac.za>
2025-07-13 22:10:30 +0200

    Fourth commit Fix all the EmployeeShift classes, interfaces and controllers

--- rms/src/main/java/controllers/EmployeeShiftControllers.java
+++ rms/src/main/java/controllers/EmployeeShiftControllers.java
@@ -1,4 +1,25 @@
 package controllers;
 
-public class EmployeeShiftControllers {
+import models.EmployeeShift;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import services.EmployeeShiftRepo;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/menuitems")public class EmployeeShiftControllers {
+    @Autowired
+    private EmployeeShiftRepo EmployeeShiftRepo;
+
+    @GetMapping({"","/"})
+    public String showMenuItems (Model model){
+        List<EmployeeShift> EmployeeShift = EmployeeShiftRepo.findAll();
+        model.addAttribute("EmployeeShift", EmployeeShift);
+        return "EmployeeShift/index";
+
+    }
 }
